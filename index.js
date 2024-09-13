19108ab86a7c3c58a145a0521d10ccf19e53a8dc
Philipp Michael Arbeitstein <philippar@edu.aau.at>
2024-09-13 11:50:47 +0200

    UI Changes

--- index.js
+++ index.js
@@ -1,8 +1,68 @@
-let num1, num2;
+let num1;
 let operation;
+let calcDone = false;
+
+// DOM ELEMENTS
+const display = document.querySelector(".display");
+const numberButtons = document.querySelectorAll(".number");
+const clearBtn = document.querySelector(".clear");
+const operatorButtons = document.querySelectorAll(".operator");
+const equalBtn = document.querySelector(".equal");
+
+equalBtn.addEventListener("click", () => {
+    const result = operate(operation, num1, Number(display.textContent));
+    display.textContent = Number.isInteger(result) ? result : result.toFixed(1);
+    num1 = result;
+    calcDone = true;
+});
+
+numberButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        if (calcDone) {
+            display.textContent = "";
+        }
+        display.textContent += "" + btn.textContent + "";
+    });
+});
+
+operatorButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        const operationBtnText = btn.textContent;
+        switch (operationBtnText) {
+            case "+":
+                num1 = Number(display.textContent);
+                clearDisplay();
+                operation = add;
+                break;
+            case "-":
+                num1 = Number(display.textContent);
+                clearDisplay();
+                operation = subtract;
+                break;
+            case "x":
+                num1 = Number(display.textContent);
+                clearDisplay();
+                operation = multiply;
+                break;
+            case "/":
+                num1 = Number(display.textContent);
+                clearDisplay();
+                operation = divide;
+                break;
+            default:
+                break;
+        }
+    });
+});
+
+clearBtn.addEventListener("click", clearDisplay);
+
+function clearDisplay() {
+    display.textContent = "";
+}
 
 function operate(operation, num1, num2) {
-    operation(num1, num2);
+    return operation(num1, num2);
 }
 
 function add(num1, num2) {
